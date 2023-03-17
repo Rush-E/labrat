bb55f96b178b01eaef9d9e77f7b20d1af1d1fd37
ijen <luyiren12@gmail.com>
2023-03-17 15:11:28 +0800

    add decorator and export

--- labrat.py
+++ labrat.py
@@ -1,36 +1,61 @@
 from git.repo import Repo
 import uuid
+import functools
 import re
 
 repo = Repo('.')
 g = repo.git
 
-try:
-    prev_branch = repo.active_branch  # acquire current branch
-except:
-    prev_branch = re.findall(r'at (.*)\n', g.status())[0]  # resolve exception caused by detached HEAD
 
-start = g.status()
+def get_current_branch():
+    try:
+        current_branch = repo.active_branch  # acquire current branch
+    except:
+        current_branch = re.findall(r'at (.*)\n', g.status())[0]  # resolve exception caused by detached HEAD
+    return current_branch
 
-g.add('.')
-g.stash()
 
-branch_name = f'labrat-tmp-branch-{uuid.uuid4().hex}'
-ref_id = uuid.uuid4().hex
+def maintain_git_workspace(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        prev_branch = get_current_branch()
+        g.add('.')
+        g.stash()
 
-g.checkout('-b', branch_name)
-g.stash('apply')
+        rv = func(*args, **kw)
 
-g.add('.')
-g.commit('-m', 'a snapshot commit created by labrat')
+        g.checkout(prev_branch)
+        g.stash('pop')
+        g.reset()
 
-g.update_ref(f"refs/labrats/{ref_id}", branch_name)
+        return rv
 
-g.checkout(prev_branch)
+    return wrapper
 
-g.branch('-D', branch_name)
 
-g.stash('pop')
-g.reset()
+def take_snapshot():
+    @maintain_git_workspace
+    def save_new_branch():
+        branch_name = f'labrat-tmp-branch-{uuid.uuid4().hex}'
+        rat_id = uuid.uuid4().hex
 
-assert start == g.status()
+        g.checkout('-b', branch_name)
+        g.stash('apply')
+
+        g.add('.')
+        g.commit('-m', 'a snapshot commit created by labrat')
+
+        g.update_ref(f"refs/labrats/{rat_id}", branch_name)
+
+        return branch_name
+
+    g.branch('-D', save_new_branch())
+
+@maintain_git_workspace
+def export_snapshot(rat_id):
+    from shutil import copytree, ignore_patterns
+    g.checkout(f"refs/labrats/{rat_id}")
+    copytree('.', f'export-rats/{rat_id}', ignore=ignore_patterns('*.git'))
+    
+
+# take_snapshot()
