ebec79bbbcd2244a139c42a3ce672520c08665e6
Kanaricc <iovo7c@gmail.com>
2023-03-17 10:04:02 +0800

    feat: use non-branch refs

--- labrat.py
+++ labrat.py
@@ -1,29 +1,26 @@
-import git
+from git.repo import Repo
 import uuid
 
-repo = git.Repo('.')
-
-
+repo = Repo('.')
 g = repo.git
-
 
 start = g.status()
 print(f'before all\n{start}')
 
 g.add('.')
-
 g.stash()
 
 # flag2
-
 branch_name = uuid.uuid4().hex
 g.checkout('-b', branch_name)
 g.stash('apply')
 
 g.add('.')
 g.commit('-m', f'message {branch_name}')
+g.update_ref(f"refs/labrats/{branch_name}",branch_name)
 
 g.checkout('master')
+g.branch('-D',branch_name)
 
 g.stash('pop')
 g.reset()
@@ -33,9 +30,3 @@ print(f'after all\n{after}')
 assert start == after
 
 print(branch_name)
-
-#
-# 123123123
-# 1231231231231
-# new change
-###
