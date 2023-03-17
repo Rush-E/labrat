52211b5b6ba290b65a515fb45aede434c69ed547
ijen <luyiren12@gmail.com>
2023-03-17 10:44:45 +0800

    record current branch

--- labrat.py
+++ labrat.py
@@ -1,32 +1,36 @@
 from git.repo import Repo
 import uuid
+import re
 
 repo = Repo('.')
 g = repo.git
 
+try:
+    prev_branch = repo.active_branch  # acquire current branch
+except:
+    prev_branch = re.findall(r'at (.*)\n', g.status())[0]  # resolve exception caused by detached HEAD
+
 start = g.status()
-print(f'before all\n{start}')
 
 g.add('.')
 g.stash()
 
-# flag2
-branch_name = uuid.uuid4().hex
+branch_name = f'labrat-tmp-branch-{uuid.uuid4().hex}'
+ref_id = uuid.uuid4().hex
+
 g.checkout('-b', branch_name)
 g.stash('apply')
 
 g.add('.')
-g.commit('-m', f'message {branch_name}')
-g.update_ref(f"refs/labrats/{branch_name}",branch_name)
+g.commit('-m', 'a snapshot commit created by labrat')
 
-g.checkout('master')
-g.branch('-D',branch_name)
+g.update_ref(f"refs/labrats/{ref_id}", branch_name)
+
+g.checkout(prev_branch)
+
+g.branch('-D', branch_name)
 
 g.stash('pop')
 g.reset()
 
-after = g.status()
-print(f'after all\n{after}')
-assert start == after
-
-print(branch_name)
+assert start == g.status()
